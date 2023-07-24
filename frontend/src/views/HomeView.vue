820c722bfa55a4b16abf801e3cc58931c0375ccc
youngjaeKim <hkm2379@gmail.com>
2023-07-24 17:08:05 +0900

    feat: [김영재] vue 회원가입, 로그인 구현 [PPF account]

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,5 +1,16 @@
 <template>
-  <hello-world />
+  <v-container>
+    <div>
+      <nav>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link> |
+        <router-link to="/product-list-page">상품 리스트</router-link> |
+        <router-link to="/account-creation-page">회원 가입</router-link> |
+        <router-link to="/account-login-page">로그인</router-link> |
+        <router-link to="/cart">장바구니</router-link> |
+      </nav>
+    </div> 
+  </v-container>
 </template>
 
 <script>
